b689da5bc78ff5de77d51a0baf5fae6b0b58cb39
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-10-30 17:13:11 +0000

    Plug-ins with the same name in PluginInfo are now distinguished by their class name in PluginNameComparator
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@468 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

--- src/de/jtem/jrworkspace/plugin/PluginNameComparator.java
+++ src/de/jtem/jrworkspace/plugin/PluginNameComparator.java
@@ -5,7 +5,13 @@ import java.util.Comparator;
 public class PluginNameComparator implements Comparator<Plugin> {
 
 	public int compare(Plugin o1, Plugin o2) {
-		return o1.getPluginInfo().name.compareTo(o2.getPluginInfo().name);
+		if (o1 == o2) {
+			return 0;
+		} else if (o1.getPluginInfo().name.equals(o2.getPluginInfo().name)) {
+			return o1.getClass().getName().compareTo(o2.getClass().getName());
+		} else {
+			return o1.getPluginInfo().name.compareTo(o2.getPluginInfo().name);
+		}
 	}
 
 }
